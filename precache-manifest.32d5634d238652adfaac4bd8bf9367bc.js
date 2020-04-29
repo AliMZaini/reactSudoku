a3965262367cbe6c2c1e736dfbf71a569cdeac6d
Ali Z <51179189+AliMZaini@users.noreply.github.com>
2020-04-29 22:46:35 +0100

    Updates

---
+++ precache-manifest.32d5634d238652adfaac4bd8bf9367bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22247972f9c46972530383b8f46a162b",
+    "url": "/reactSudoku/index.html"
+  },
+  {
+    "revision": "56fdb1bfa2cfb62d8640",
+    "url": "/reactSudoku/static/css/main.f86112e2.chunk.css"
+  },
+  {
+    "revision": "8a7cc772241beda50cf7",
+    "url": "/reactSudoku/static/js/2.9ec45e9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactSudoku/static/js/2.9ec45e9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56fdb1bfa2cfb62d8640",
+    "url": "/reactSudoku/static/js/main.975fc0fe.chunk.js"
+  },
+  {
+    "revision": "8fbd73c952333a96fba3",
+    "url": "/reactSudoku/static/js/runtime-main.f8220162.js"
+  }
+]);
